3393a191bb4620bc0d69da1dd220866a5afec115
rocawear <anttivep@gmail.com>
2023-03-14 00:11:26 +0200

    story

--- packages/loiste-ui/src/components/Button/Button.tsx
+++ packages/loiste-ui/src/components/Button/Button.tsx
@@ -7,13 +7,7 @@ const button = cva('rounded-md relative', {
     variant: {
       default: 'bg-white text-text shadow-button border border-gray-400',
       primary: 'bg-primary text-white',
-      destructive: 'bg-destructive text-white',
       monochrome: 'bg-white text-text border border-gray-400',
-    },
-    modifier: {
-      outline:
-        'bg-transparent border border-current shadow-[0_0_0_1px_currentColor]',
-      plain: 'shadow-none bg-transparent border-none px-2 py-1 text-sm',
     },
     size: {
       slim: 'text-sm',
@@ -29,52 +23,21 @@ const button = cva('rounded-md relative', {
   },
   compoundVariants: [
     {
-      modifier: 'outline',
-      variant: 'destructive',
-      className: 'text-destructive',
-    },
-    {
-      modifier: 'outline',
       variant: 'primary',
       className: 'text-primary',
     },
+
     {
-      modifier: 'plain',
-      variant: 'destructive',
-      className: 'text-destructive',
-    },
-    {
-      modifier: 'plain',
-      variant: 'primary',
-      className: 'text-primary',
-    },
-    {
-      modifier: undefined,
       size: 'slim',
       className: 'px-3 py-[3px]',
     },
+
     {
-      modifier: 'outline',
-      size: 'slim',
-      className: 'px-3 py-[3px]',
-    },
-    {
-      modifier: undefined,
       size: 'medium',
       className: 'px-4 py-2',
     },
+
     {
-      modifier: 'outline',
-      size: 'medium',
-      className: 'px-4 py-2',
-    },
-    {
-      modifier: undefined,
-      size: 'large',
-      className: 'px-6 py-3',
-    },
-    {
-      modifier: 'outline',
       size: 'large',
       className: 'px-6 py-3',
     },
@@ -100,16 +63,13 @@ export interface ButtonProps
 export const Button = ({
   children,
   variant,
-  modifier,
   size,
   fullWidth,
   disabled,
   loading = false,
 }: ButtonProps) => {
   return (
-    <button
-      className={button({ variant, modifier, size, fullWidth, disabled })}
-    >
+    <button className={button({ variant, size, fullWidth, disabled })}>
       <span
         className={classNames(
           'leading-[1.25rem]',
